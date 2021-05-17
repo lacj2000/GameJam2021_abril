d555b826f223662b52fc785db0313d50af91ce27
PhePheMath <PhelyppeMath@outlook.com>
2021-05-17 13:30:42 -0300

    Debug achievements

--- AhManoMitocondria/achievements.gd
+++ AhManoMitocondria/achievements.gd
@@ -1,20 +1,24 @@
+extends Node
 const strings = preload("res://strings.gd") 
+var language = get_node("Main").language
 
 var achievements = {
 	"atp": {
-		1: strings["achievement_0001"]},
-		10 strings["achievement_0002"],
-		100: strings["achievement_0003"],
-		1000: strings["achievement_0004"],
-		10000: strings["achievement_0005"],
-		100000: strings["achievement_0006"],
-		1000000: strings["achievement_0007"],
-		10000000: strings["achievement_0008"],
-		100000000: strings["achievement_0009"]
+		1: strings.achievement_0001[language],
+		10: strings.achievement_0002[language],
+		100: strings.achievement_0003[language],
+		1000: strings.achievement_0004[language],
+		10000: strings.achievement_0005[language],
+		100000: strings.achievement_0006[language],
+		1000000: strings.achievement_0007[language],
+		10000000: strings.achievement_0008[language],
+		100000000: strings.achievement_0009[language]
 	},
 	"mitose": {
-		10: strings['achievement_0010'],
-		100: strings['achievement_0011'],
-		1000: strings['achievement_0012']
+		10: strings.achievement_0010[language],
+		100: strings.achievement_0011[language],
+		1000: strings.achievement_0012[language]
 	}
 }
+func _ready():
+	print(strings.achievement_0012[language])
