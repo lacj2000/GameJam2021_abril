0c01fba03dd614d32b9bdd4f4f3ef0a997d04748
PhePheMath <PhelyppeMath@outlook.com>
2021-05-17 01:26:24 -0300

    Strings de achievement

---
+++ AhManoMitocondria/strings.gd
@@ -0,0 +1,49 @@
+var strings = {
+	"achievement_0001": [
+		"Vai logo, funciona",
+		"C'mon, work already"],
+	"achevement_0002": [
+		"Não fez mais que a obrigação",
+		"Nothing to be proud at all"
+	],
+	"achievement_0003": [
+		"Ora ora, parece que temos um bom candidato",
+		"Oh my my, it seem we have something worth here"
+	],
+	"achievement_0004": [
+		"Isso, trabalhe bastante, para mim.",
+		"You better you full my pockets, got it?"
+	],
+	"achievement_0006": [
+		"Eles nem entendem o potêncial da minha pesquisa",
+		"They can't even begin to understand what you're worth"
+	],
+	"achievement_0007": [
+		"Tudo correndo bem",
+		"Everything according to my plans"
+	],
+	"achievement_0008": [
+		"Eles querem resultados, RÁPIDO!! RÁPIDO!!!",
+		"They're getting impatient, FASTER!! FASTER!!!"
+	],
+	"achievement_0009": [
+		"Aqueles investidores não entendem nada, você vai crescer e me vingar, eu sei disso",
+		"Those old geezers know nothing, you'll be my revenge, grow well"
+	],
+	"achievement_0010": [
+		"Isso, se multipliquem",
+		"Yeah, keep spliting"
+	],
+	"achievement_0011": [
+		"Parece que esses aqui deram certo",
+		"It seems this bath is doing well"
+	],
+	"achievement_0012": [
+		"Sua evolução será minha vitória",
+		"If you thrive, I thrive"
+	],
+	"achievement_0013": [
+		"Um exército, é isso que vocês serão!!!",
+		"My own army, can't wait for you to be complete"
+	]
+}
